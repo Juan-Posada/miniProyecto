066acadfc7449fcff4ae1f95b3033b162e580c47
Juan-Posada <juanposada.0724@gmail.com>
2024-06-20 11:53:51 -0500

    Progreso 11:53 20/06/2024

--- miniProyecto.dart
+++ miniProyecto.dart
@@ -1,11 +1,23 @@
+import 'dart:io';
+
 void main(){
   //Mini proyecto final trimestre II - Juan Posada
 
   //DEFINICIÓN DE VARIABLES
-  int trabajo = 18;
-  List<int> trabajos = [2, 5, 6];
+  List<String> nombres = [];
+  int opcionMenu;
 
   //PROCESOS
-  print(trabajo);
-  print(trabajos[0]);
+
+  do {
+  print("MENU APP");
+  print("-"*25);
+  print("1. Ingresar estudiantes");
+  print("2. Crear temas de exposición");
+
+  print("*"*30);
+  print("Digite la opción deseada");
+  opcionMenu = int.parse(stdin.readLineSync()!);
+  
+  } while (opcionMenu != 0);
 }
