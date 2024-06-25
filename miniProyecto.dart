8e3674211f75b29f1d533c28146077a8353c3b0e
Juan-Posada <juanposada.0724@gmail.com>
2024-06-25 07:01:35 -0500

    Push quedado clase

--- miniProyecto.dart
+++ miniProyecto.dart
@@ -5,19 +5,99 @@ void main(){
 
   //DEFINICIÓN DE VARIABLES
   List<String> nombres = [];
-  int opcionMenu;
+  List<List<String>> exposicionesTemas = [];
+  int opcionMenuPrincipal, opcionMenuEstudiantes, opcionMenuTemas, opcionMezcla;
 
   //PROCESOS
 
   do {
-  print("MENU APP");
-  print("-"*25);
-  print("1. Ingresar estudiantes");
-  print("2. Crear temas de exposición");
+  print("");
+  print("[BIENVENIDO]");
+  print("🏁"*30);
+  print("MENÚ APP PRINCIPAL");
+  print("🏁"*30);
+  print("1. Entrar al menú estudiantes");
+  print("2. Entrar al menú de exposicones");
+  print("3. Mezclar grupos de trabajo");
+  print("0. Salir");
 
-  print("*"*30);
+  print("-"*30);
   print("Digite la opción deseada");
-  opcionMenu = int.parse(stdin.readLineSync()!);
+  opcionMenuPrincipal = int.parse(stdin.readLineSync()!);
   
-  } while (opcionMenu != 0);
+  switch (opcionMenuPrincipal) {
+    case 1:
+      do {
+          print("");
+          print("🏁"*30);
+          print("MENÚ CRUD ESTUDIANTES");
+          print("🏁"*30);
+          print("1. Crear lista de estudiantes");
+          print("2. Modificar lista de estudiantes");
+          print("3. Mostrar lista de estudiantes");
+          print("0. Salir");
+
+          print("-"*30);
+          print("Digite la opción deseada");
+          opcionMenuEstudiantes = int.parse(stdin.readLineSync()!);
+
+          switch (opcionMenuEstudiantes) {
+            case 1:
+              
+              break;
+
+            case 2:
+
+              break;
+
+            case 3:
+            
+              break;  
+
+            default:
+
+              break;
+          }
+      } while (opcionMenuEstudiantes != 0);
+      break;
+    
+    case 2:
+      do {
+          print("");
+          print("🏁"*30);
+          print("MENU CRUD TEMAS DE EXPOSICIÓN");
+          print("🏁"*30);
+          print("1. Crear lista de temas de exposición");
+          print("2. Modificar lista de temas de exposición");
+          print("3. Mostrar lista de temas de exposición");
+          print("0. Salir");
+
+          print("-"*30);
+          print("Digite la opción deseada");
+          opcionMenuTemas = int.parse(stdin.readLineSync()!);
+      } while (opcionMenuTemas != 0);
+      break;
+
+    case 3:
+      do {
+          print("");
+          print("🏁"*30);
+          print("MENU CRUD MEZCLA DE GRUPOS");
+          print("🏁"*30);
+          print("1. Mezclar equipos de trabajo");
+          print("2. Mostrar equipos de trabajo");
+          print("0. Salir");
+
+          print("*");
+          print("Digite la opción deseada");
+          opcionMezcla = int.parse(stdin.readLineSync()!);
+      } while (opcionMezcla != 0);
+      break;
+
+    default:
+      print("¡Hasta pronto, 🙌!");
+      break;
+  }
+
+  } while (opcionMenuPrincipal != 0);
 }
